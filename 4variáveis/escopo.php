59666e14c4fe7b59108e5c8205fb378c13b16c06
Victor Martins <victorgnr2010@hotmail.com>
2024-09-06 17:04:49 -0300

    update escopo.php

---
+++ 4variáveis/escopo.php
@@ -0,0 +1,63 @@
+<?php
+//ESCOPO GLOBAL
+// $teste = "victor"; 
+
+// echo "$teste global 1 <br>";
+
+// if(5 > 2) {
+//     $teste = "carla";
+//     echo "$teste if 1 <br>";
+// }
+
+// echo "$teste global 2 <br>";
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//ESCOPO LOCAL
+
+// $x = 10;
+
+// echo "$x global <br>";
+
+// function teste() {
+//     $x = 5;
+//     echo "$x local <br>";
+// }
+
+// teste();
+
+// echo "$x global <br>";
+
+// teste();
+
+// function testando() {
+//     $x = 12; 
+//     echo "$x local 2 <br>";
+// }
+
+// $x = 99; 
+
+// echo "$x global <br>";
+
+// teste();
+// testando();
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//ESCOPO STATIC
+
+
+
+
+
+
+
+
+
+//ESCOPO DOS PARAMETROS
+
+
+
+
+
+
+
+
+
+?>
