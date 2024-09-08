25b6e728bff3672b462a107e4a73e33025d09632
Victor Martins <victorgnr2010@hotmail.com>
2024-09-08 12:23:42 -0300

    Praticando

---
+++ 5operadores/op_de_comparacao.php
@@ -0,0 +1,19 @@
+<?php
+
+if(5 == 5) {
+    echo "É verdade <br>";
+}
+
+if(5 == 3) {
+    echo "É verdade 1 <br>";
+}
+
+if (5 === "5") {
+    echo "É verdade 2 <br>";
+}
+
+if(5 === 5) {
+    echo "É verdade 3 <br>";
+}
+
+?>
