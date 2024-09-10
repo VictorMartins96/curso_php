8a7f7191ee7e4805a7484d02c95cef956bc13798
Victor Martins <victorgnr2010@hotmail.com>
2024-09-10 15:38:09 -0300

    if else

---
+++ 6estruturas_de_controle/estrutura_else.php
@@ -0,0 +1,30 @@
+<?php
+
+if (5 > 2) {
+    echo "entrou no if";
+} else {
+    echo "entrou no else";
+}
+echo "<hr>";
+
+if("teste" === 5) {
+    echo "entrou no if";
+} else {
+    echo "entrou no else <hr>";
+}
+
+// variaveis 
+
+$a = 4;
+$b = 5;
+
+$msg = "entrou no else <hr>";
+
+if($a > $b) {
+    
+} else {
+    echo $msg;
+}
+
+
+?>
