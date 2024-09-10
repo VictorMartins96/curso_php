6eb61a742d7b17a0c09e8feda08a91e69877b0e4
Victor Martins <victorgnr2010@hotmail.com>
2024-09-09 22:18:21 -0300

    operadores cast

---
+++ 5operadores/op_de_cast.php
@@ -0,0 +1,26 @@
+<?php
+
+$a = (int) "12";
+$b = (float) "3.164621";
+$c = (string) 34;
+
+echo $a;
+echo "<br>";
+
+echo $a + 10;
+echo "<br>";
+
+if($a === 12) {
+    echo "É identico a 12 <br>";
+}
+
+if($b === 3.164621) {
+    echo "É identico <br>";
+}
+
+if($c === "34") {
+    echo "É identico 1 <br>";
+}
+
+
+?>
