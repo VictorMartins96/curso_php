98744bdcc987109af2853e14b548e9177928989a
Victor Martins <victorgnr2010@hotmail.com>
2024-09-10 15:49:29 -0300

    exercicio 23

---
+++ 6estruturas_de_controle/exercicio23.php
@@ -0,0 +1,43 @@
+<?php
+
+$nome = "Victor";
+$idade = 28;
+$peso = 85.1;
+$possuicnh = true;
+
+if(is_int($nome)) {
+    echo "É inteiro <br>";
+} else {
+    echo "Não é inteiro <br>";
+}
+echo "<hr>";
+
+if(is_int($idade)) {
+    echo "É inteiro <br>";
+} else {
+    echo "Não é inteiro <br>";
+}
+echo "<hr>";
+
+if(is_int($peso)) {
+    echo "É inteiro <br>";
+} else {
+    echo "Não é inteiro <br>";
+}
+echo "<hr>";
+
+if(is_int($possuicnh)) {
+    echo "É inteiro <br>";
+} else {
+    echo "Não é inteiro <br>";
+}
+
+
+
+
+
+
+
+
+
+?>
