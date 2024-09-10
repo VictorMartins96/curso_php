acd0a4fafec895e32a2215f633f9e7786f6a9bcf
Victor Martins <victorgnr2010@hotmail.com>
2024-09-10 19:19:38 -0300

    else if

---
+++ 6estruturas_de_controle/estrutura_elseif.php
@@ -0,0 +1,33 @@
+<?php
+
+if(10 > 5) {
+    
+    echo "Entrou no if <br>";
+
+} else if (10 > 6) {
+    
+    echo "Entrou no else if <br>";
+
+}
+
+if(10 < 5) {
+    
+    echo "Entrou no if <br>";
+
+} else if (10 < 6) {
+    
+    echo "Entrou no else if <br>";
+
+} else if ( 11 < 1 ) {
+    
+    echo "Entrou no else if 2 <br>";
+
+} else {
+
+    echo "Entrou no else";
+
+}
+
+
+
+?>
