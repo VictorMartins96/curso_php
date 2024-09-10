deae230dec7bf8b37133ba4ecf0af0d713ffe2dc
Victor Martins <victorgnr2010@hotmail.com>
2024-09-10 01:22:51 -0300

    if exercicios

---
+++ 6estruturas_de_controle/exercicio21.php
@@ -0,0 +1,33 @@
+<?php
+
+$num1 = 5;
+$num2 = 2;
+$nome1 = "Victor";
+$nome2 = "Carla";
+$num3 = 12;
+$num4 = 11;
+
+
+if ( $num1 > $num2) {
+    echo "Imprima se for verdadeiro 1 <hr>";
+}
+
+if($nome1 !== $nome2) {
+    echo "Imprima se os nomes forem diferentes";
+}
+
+if($num3 <= $num4) {
+    echo "Imprima se 12 for menor ou igual a 11";
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ 6estruturas_de_controle/exercicio22.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$idade1 = 12;
+$idade2 = 15;
+$idade3 = 21;
+$idade4 = 39;
+$idade5 = 56;
+
+
+if ($idade3 >= 18 && $idade3 <= 55) {
+    echo "Você é maior de idade, porém não é idoso <hr>";
+}
+
+if($idade5 >= 55) {
+    echo "Você é idoso";
+}
+
+
+
+?>
