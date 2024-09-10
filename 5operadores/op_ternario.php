92ae770bd1cad27d546b8fe2797294c528419f1d
Victor Martins <victorgnr2010@hotmail.com>
2024-09-10 01:00:12 -0300

    op ternario

---
+++ 5operadores/op_ternario.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$a = 25;
+$b = 40;
+
+echo $a > $b ? "Verdadeiro" : "Falso";
+
+echo "<br>";
+
+echo $a < $b ? "Verdadeiro" : "Falso";
+
+echo "<br>";
+
+echo $b === $a ? "Verdadeiro" : "Falso";
+
+
+?>
