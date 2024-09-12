9c745b3ecea4103f704532d92e4f2003e2db6598
Victor Martins <victorgnr2010@hotmail.com>
2024-09-12 11:20:40 -0300

    training

--- 6estruturas_de_controle/estrutura_switch.php
+++ 6estruturas_de_controle/estrutura_switch.php
@@ -1,18 +1,44 @@
 <?php
 
-$x = 80;
+// $x = 80;
 
-switch($x) {
-    case 0:
-        echo "X é igual a 0 <br>";
-        break;
-    case 1:
-        echo "X é igual a 1 <br>";
-        break;
-    default: 
-        echo "o X não é nenhum destes valores <br>";
+// switch($x) {
+//     case 0:
+//         echo "X é igual a 0 <br>";
+//         break;
+//     case 1:
+//         echo "X é igual a 1 <br>";
+//         break;
+//     default: 
+//         echo "o X não é nenhum destes valores <br>";
+// }
+
+function calcularDesconto($produto , $categoria) {
+
+    $desconto = 0;
+
+    switch($categoria) {
+        case 'eletrônicos':            
+            $produto - $desconto;
+            break;
+        case 'vestuário':
+            $desconto = 20;
+            $produto - $desconto;
+            break;
+        case 'alimentos':
+            $desconto = 5;
+            $produto - $desconto;
+            break;
+        case 'Outras categorias':
+            $desconto = 0;
+            $produto - $desconto;
+            break;
+        default:
+            echo "Categoria desconhecida, não há descontos";
+    }
 }
 
+calcularDesconto( 100 , 'alimentos');
 
 
 
