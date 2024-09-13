7c889712f7d91d1822a0d1bb1b91bbab130ae346
Victor Martins <victorgnr2010@hotmail.com>
2024-09-12 22:56:38 -0300

    break

---
+++ 7estrutura_de_repetição/break.php
@@ -0,0 +1,24 @@
+<?php
+
+$x = 0;
+
+while ($x <= 10) {
+
+    echo $x . "<br>";
+
+    if($x == 5) {
+        echo "Terminando o loop <br>";
+        break;
+    }
+
+    $x++;
+
+}
+
+//exercicios
+
+
+
+
+
+?>
