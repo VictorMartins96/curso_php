f4e0482aa56784bfe28a0dd5c35d777e4feadbe8
Victor Martins <victorgnr2010@hotmail.com>
2024-09-12 23:16:19 -0300

    loop aninhado

---
+++ 7estrutura_de_repetição/loop_aninhado.php
@@ -0,0 +1,24 @@
+<?php
+
+$externo = 0; 
+
+while($externo <= 10) {
+
+    echo "Loop externo $externo <br>";
+    $externo++;
+
+    $interno = 1;
+
+    while($interno <= 7) {
+        echo "Loop interno $interno <br>";
+        $interno++;
+    }
+
+}
+
+
+
+
+
+
+?>
