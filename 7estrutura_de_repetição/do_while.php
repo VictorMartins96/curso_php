8753c1c068e7b5012120d1d95ed76db7555c6159
Victor Martins <victorgnr2010@hotmail.com>
2024-09-14 19:31:28 -0300

    update

--- 7estrutura_de_repetição/do_while.php
+++ 7estrutura_de_repetição/do_while.php
@@ -1,13 +1,29 @@
 <?php
 
-$x = 0;
+// $x = 0;
+
+// do {
+
+//     echo "Testando do while $x <br>";
+//     $x++;
+
+// } while($x <= 10)
+
+
+$x = 100;
 
 do {
 
-    echo "Testando do while $x <br>";
-    $x++;
+    if($x == 55) {
+        echo "Pulou a execução $x <br>";
+        $x--;  //o decremento vem antes do continue
+        continue;
+    }
+    
+    echo "Testando do while decremento $x <br>";
+    $x--;
 
-} while($x <= 10)
+} while ($x > 0)
 
 
 
