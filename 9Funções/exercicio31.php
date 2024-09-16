c23ca347d2dada6cd45a50f34e8d61e44d1271ec
Victor Martins <victorgnr2010@hotmail.com>
2024-09-16 00:08:16 -0300

    exercicio31

---
+++ 9Funções/exercicio31.php
@@ -0,0 +1,26 @@
+<?php
+
+
+// $arr = ["Victor" , "Martins" , "Moreira" , "Alves"];
+
+// $fun = implode(", " , $arr);
+
+// echo $fun;
+
+// function multi($n1 , $n2 , $n3) {
+//     echo $n1 * $n2 . "<br>";
+//     echo $n2 * $n3 . "<br>";
+//     echo $n1 * $n3 . "<br>";
+// }
+
+// multi(30 , 30 , 10);
+
+
+function names($nome , $sobrenome) {
+    echo "Olá, me chamo $nome  $sobrenome <br>";
+}
+
+names("Victor" , "Martins");
+
+
+?>
