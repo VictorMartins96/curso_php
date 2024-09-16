8307c64af73ab45ca69d288acb39bfa42f426b23
Victor Martins <victorgnr2010@hotmail.com>
2024-09-16 19:15:43 -0300

    exercitando

--- 9Funções/parametro.php
+++ 9Funções/parametro.php
@@ -1,32 +1,58 @@
 <?php
 
-function velMax($vel) {
+// function meuNome($nome , $idade) {
 
-    if(is_int($vel)) {
-        echo "O carro atinge a velocidade máxima de $vel <br>";
-    } else {
-        echo "Por favor, digite um numero inteiro <br>";
-    }
+//     echo "Olá, meu nome é $nome e tenho $idade anos <br>";
+    
+// }
+
+// meuNome("Victor" , 28);
+
+function parouimpar($num) {
+
+    echo $num % 2 == 0 ? "Este numero é par <br>" : "Este numero é impar <br>";
 
 }
 
-velMax(200);
+parouimpar(30);
 
-//Não pode executar a chamada sem passar o parametro
 
-$velocidade = 125;
 
-velMax($velocidade);
 
-//php ignora parametro desnecessario
 
-velMax("teste");
 
-function descreverAnimal($nome , $raca) {
-    echo "O nome $nome é da raça $raca";
-}
 
-descreverAnimal('bob' , 'pitbull');
+
+
+
+
+// function velMax($vel) {
+
+//     if(is_int($vel)) {
+//         echo "O carro atinge a velocidade máxima de $vel <br>";
+//     } else {
+//         echo "Por favor, digite um numero inteiro <br>";
+//     }
+
+// }
+
+// velMax(200);
+
+// Não pode executar a chamada sem passar o parametro
+
+// $velocidade = 125;
+
+// velMax($velocidade);
+
+// php ignora parametro desnecessario
+
+// velMax("teste");
+
+// function descreverAnimal($nome , $raca) {
+//     echo "O nome $nome é da raça $raca";
+// }
+
+// descreverAnimal('bob' , 'pitbull');
 
 
 
