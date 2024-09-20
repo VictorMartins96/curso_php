2b180426218e8c434598599c209ff2bab76a4338
Victor Martins <victorgnr2010@hotmail.com>
2024-09-20 11:01:01 -0300

    default php

---
+++ 9Funções/argumentodefault.php
@@ -0,0 +1,41 @@
+<?php
+
+//Exercicio
+
+function defineCorCarro()
+
+
+
+
+
+
+
+
+// function teste($a = "teste") {
+
+//     echo "O valor de a é: $a <br>";
+
+
+// }
+// teste();
+// teste("victor");
+
+// function testando($b , $a = "x") {
+
+//     echo "O valor A é: $a , e o valor B é: $b <br>";
+
+
+// }
+// testando("15");
+// testando("a" , "b");
+
+
+
+
+
+
+
+
+
+
+?>
