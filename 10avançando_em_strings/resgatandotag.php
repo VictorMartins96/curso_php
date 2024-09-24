8a34cb36bfe68f00d1d4ee704489099a8a4120e2
Victor Martins <victorgnr2010@hotmail.com>
2024-09-24 20:52:43 -0300

    resgatando string

---
+++ 10avançando_em_strings/resgatandotag.php
@@ -0,0 +1,17 @@
+<?php
+
+$str = "Esta é a minha string";
+
+$minha = substr($str , 16 , 6); // String pai, indice inicial , comprimento
+
+echo $str . "<br>";
+
+echo $minha . "<br>";
+
+$str2 = "Testando uma nova string";~
+
+$nova = substr($str2 , 9);
+
+echo $str2 . "<br>";
+
+echo $nova . "<br>";
