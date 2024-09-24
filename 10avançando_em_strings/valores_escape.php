1db019ebe80642702ed354b0a32e9c6a8bb807bc
Victor Martins <victorgnr2010@hotmail.com>
2024-09-24 11:33:48 -0300

    valores escape

---
+++ 10avançando_em_strings/valores_escape.php
@@ -0,0 +1,24 @@
+<?php
+
+header("Content-type: text/plain");
+
+// pular linha
+
+echo "Isso vai ficar na primeira linha \n e isso vai ficar na segunda linha \n";
+
+//tab 
+
+echo "Testando o tab \t aqui \t \n";
+
+//barra invertida 
+
+echo "Testando barra invertida \\ \n";
+
+//dolar
+
+echo "Imprimindo o dolar \$teste \n";
+
+
+
+
+?>
