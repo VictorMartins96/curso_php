24b2699d312d815a901845ebc158a474cdce0eeb
Victor Martins <victorgnr2010@hotmail.com>
2024-09-24 17:48:02 -0300

    ex41

---
+++ 10avançando_em_strings/ex41.php
@@ -0,0 +1,16 @@
+<?php
+
+$str = "este item está em ";
+$str2 = "promoção";
+
+echo ucfirst($str) . strtoupper($str2);
+
+// function alter() {
+    
+
+
+// }
+
+// $new = alter($str , $str2);
+
+// echo $new;
