ac03335ead470b2e1f0d80a3bd510d6785c7d4d4
Victor Martins <victorgnr2010@hotmail.com>
2024-09-24 21:16:59 -0300

    arraystring

---
+++ 10avançando_em_strings/arraystring.php
@@ -0,0 +1,13 @@
+<?php
+
+// $frase = ["Victor" , "Martins" , "Moreira" , "Alves"];
+
+// $new = implode(" ", $frase);
+
+// echo "$new .<br>";
+
+$arr = ["O" , "PHP" , "é" , "muito" , "legal"];
+
+$new = implode(" ", $arr);
+
+echo "Está escrito: $new";
