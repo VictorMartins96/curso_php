bc5a47deaa7f5aa82c4228d0a05fbd0cced1d501
Victor Martins <victorgnr2010@hotmail.com>
2024-09-26 11:54:33 -0300

    ex46

---
+++ 11avançando_em_arrays/ex46.php
@@ -0,0 +1,27 @@
+<?php
+
+$arr = [
+    [1 , 2 , 3 , 4],
+    [5 , 6 , 7 , 8],
+    [9 , 10 , 11 , 12]
+];
+
+echo $arr[0][0] . "<br>";
+echo $arr[0][1] . "<br>";
+echo $arr[0][2] . "<br>";
+echo $arr[0][3] . "<br>";   
+echo "Mudando de array!";
+echo "<br>";
+
+echo $arr[1][0] . "<br>";
+echo $arr[1][1] . "<br>";
+echo $arr[1][2] . "<br>";
+echo $arr[1][3] . "<br>";   
+echo "Mudando de array!";
+echo "<br>";
+
+echo $arr[2][0] . "<br>";
+echo $arr[2][1] . "<br>";
+echo $arr[2][2] . "<br>";
+echo $arr[2][3] . "<br>";   
+
