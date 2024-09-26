623c0c3972f42709aa7548a17cbcfc63bf256b14
Victor Martins <victorgnr2010@hotmail.com>
2024-09-26 11:58:02 -0300

    muitas variaveis

--- 11avançando_em_arrays/muitasvariaveis.php
+++ 11avançando_em_arrays/muitasvariaveis.php
@@ -1,2 +1,13 @@
 <?php
 
+$pessoa = ["Victor" , 28 , "Dev" , "preto"];
+
+print_r($pessoa);
+echo "<br>";
+
+list($nome , $idade , $profissao , $cor) = $pessoa;
+
+echo $nome . "<br>";
+echo $idade . "<br>";
+echo $profissao . "<br>";
+echo $cor . "<br>";
