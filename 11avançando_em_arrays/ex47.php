04036b2a23577d8eb77ae067bdc755493a1c9670
Victor Martins <victorgnr2010@hotmail.com>
2024-09-26 12:03:21 -0300

    ex47

---
+++ 11avançando_em_arrays/ex47.php
@@ -0,0 +1,12 @@
+<?php
+
+$carro = ["Jaguar" , 3.0 , "Azul" , 18 , "Teto-solar", "Automático"];
+
+list($nomeCarro, $motor , $cor , $aro , $teto , $cambio) = $carro;
+
+echo $nomeCarro . "<br>";
+echo $motor . "<br>";
+echo $cor . "<br>";
+echo $aro. "<br>";
+echo $teto . "<br>";
+echo $cambio . "<br>";
