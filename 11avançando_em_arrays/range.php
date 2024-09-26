81bf7674d543d56821a3794a4fd7d082f69ffac1
Victor Martins <victorgnr2010@hotmail.com>
2024-09-26 11:28:20 -0300

    range

---
+++ 11avançando_em_arrays/range.php
@@ -0,0 +1,12 @@
+<?php
+//criando array de forma rápida com a função range
+
+$arr = range(5 , 50);
+
+print_r($arr);
+echo "<br>;";
+
+$arr2 = range(1 , 1000, 100/*passo do elemento*/); 
+
+print_r($arr2);
+echo "<br>;";
