2893327ebf478ba250a1a447a850faeb36aba396
Victor Martins <victorgnr2010@hotmail.com>
2024-10-10 10:43:38 -0300

    validaform

---
+++ 14PHP_e_web/validacaoformulario.php
@@ -0,0 +1,51 @@
+<?php
+
+$validacoes = [];
+    
+    if(count($_POST) > 0) {
+        
+        if(!$_POST['nome'] === "") {
+            $validacoes[] = "Por favor, preencha o nome do usuário! <br>";
+        }
+        if(!$_POST['email'] === "") {
+            $validacoes[] = "Por favor, preencha o email do usuário! <br>";
+        }
+        if($_POST['senha'] != $_POST['confirmacao']) {
+            $validacoes[] = "As senhas devem ser iguais! <br>";
+        }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<?php if(count($validacoes)): ?>
+    <ul>
+        <?php foreach($validacoes as $validacao): ?>
+            <li><?= $validacao ?></li>
+        <?php endforeach; ?>    
+    </ul>
+<?php endif; ?>    
+<body>
+    <form action="validacaoformulario.php" method="POST">
+        <div>
+            <input type="text" name="nome" placeholder="Digite seu nome">
+        </div>
+        <div>
+            <input type="text" name="email" placeholder="Digite seu email">
+        </div>
+        <div>
+            <input type="password" name="senha" placeholder="Digite sua senha">
+        </div>
+        <div>
+            <input type="password" name="confirmacao" placeholder="Confirmação de senha">
+        </div>
+        <div>
+            <input type="submit" value="Enviar">
+        </div>
+    </form>
+</body>
+</html>
